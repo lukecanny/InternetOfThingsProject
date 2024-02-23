f2a758e24fd4ff24fce676689ffdaf5eb11c873d
lukecanny <lcanny8@gmail.com>
2024-02-23 15:53:13 +0000

    added comments, refactored how data is loaded

--- feather-sense/deliverable_1.py
+++ feather-sense/deliverable_1.py
@@ -1,71 +1,83 @@
-# Code for Feather Sense for Deliverable #1
+# Code for Feather Sense Deliverable #1
 # Authors: Ciaran Harris, Luke Canny
 # Date: 23/02/24
 
+### Library Imports
 import time
 import digitalio
 import board
-
-
-# Import the Adafruit Bluetooth library.  Technical reference:
-# https://circuitpython.readthedocs.io/projects/ble/en/latest/api.html
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.nordic import UARTService
 
-# ----------------------------------------------------------------
-# Initialize global variables for the main loop.
+### Declare Global Variables
+# LED 
 ledpin= digitalio.DigitalInOut(board.BLUE_LED)
 ledpin.direction = digitalio.Direction.OUTPUT
 
+# Bluetooth
 ble = BLERadio()
 uart = UARTService()
 advertisement = ProvideServicesAdvertisement(uart)
 
-# Flags for detecting state changes.
+# Status Flags
 advertised = False
 connected  = False
 
-# The sensor sampling rate is precisely regulated using the following timer variables.
+# Timing (Sampling Rate) Variables
 sampling_timer    = 0.0
 last_time         = time.monotonic()
 sampling_interval = 1
 
-# CSV File 
-file_path = 'sample_data.csv'
+# Output CSV 
+file_path = 'sample_data.csv'   # Input File Name - Simulated Sensor Data
+index = 0                       # Index in file (Row)
 
+### Set Up Code
+# Open Sample Sensor Data
 csv_file = open(file_path, 'r')
 entire_file = csv_file.read()
 rows = entire_file.split("\n")
-index = 0
-# csv_reader = csv.reader(csv_file)
 
-# ----------------------------------------------------------------
-# Begin the main processing loop.
-
+### Main Loop
 while True:
-    # Read the accelerometer at regular intervals.  Measure elapsed time and
-    # wait until the update timer has elapsed.
+    
+    # Check the time
     now = time.monotonic()
     interval = now - last_time
     last_time = now
     sampling_timer -= interval
+    # If the interval is greater than sampling time,
+    #   set the next simulated value
     if sampling_timer < 0.0:
         sampling_timer += sampling_interval
-        x, y, z = (10,20,30)
+        if index < len(rows):
+            x = rows[index]
+            index = index + 1
+        else:
+            index = 0
+            x = f"{rows[index]}"
+            index = index + 1
     else:
         x = None
 
+    # If the bluetooth is not advertising - Begin advertising
     if not advertised:
         ble.start_advertising(advertisement)
         print("Waiting for connection.")
         advertised = True
 
+    # If conected flag is not enabled, but bluetooth is connected,
+    #   Change the flag, turn on LED, and print message
     if not connected and ble.connected:
         print("Connection received.")
         connected = True
         ledpin.value = True
-        
+    
+    # If connected (flag) is true:
+    #   Check if connection is still active
+    #       If not, mark connection is lost and reset flags
+    #   Otherwise, print the data to bluetooth!
     if connected:
         if not ble.connected:
             print("Connection lost.")
@@ -74,11 +86,5 @@ while True:
             ledpin.value = False            
         else:
             if x is not None:
-                if index < len(rows):
-                    uart.write(rows[index])
-                    index = index + 1
-                else:
-                    index = 0
-                    uart.write(f"{rows[index]}")
-                    index = index + 1
+                uart.write(x)
                 
